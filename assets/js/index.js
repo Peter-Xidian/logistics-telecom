620af68a765b7d3cebfbcc768f3411dcde2a46e4
Peter-Xidian <mayowaodebode@gmail.com>
2023-07-25 20:24:40 +0200

    JS Commit

--- assets/js/index.js
+++ assets/js/index.js
@@ -44,57 +44,42 @@ function linkAction() {
 }
 navLink.forEach((n) => n.addEventListener("click", linkAction));
 
-/*==================== EVENTS CAROUSEL ====================*/
-const arrows = document.querySelectorAll("[data-carousel-arrow]");
+/*==================== TOGGLE MENU ====================*/
+// Get all sidebar elements and containers
+const sidebarlink = document.querySelectorAll(".sn");
+const containers = document.querySelectorAll(".ltc");
 
-arrows.forEach((arrow) => {
-  arrow.addEventListener("click", () => {
-    const offset = arrow.dataset.carouselArrow === "next" ? 1 : -1;
-    const slides = arrow
-      .closest("[data-carousel]")
-      .querySelector("[data-slides]");
+// Add a click event listener to each sidebar element
+sidebarlink.forEach((element, index) => {
+  element.addEventListener("click", () => {
+    // Remove active class from all sidebar link
+    sidebarlink.forEach((el) => {
+      el.classList.remove("active");
+    });
+    // Add active class to the clicked sidebar element
+    element.classList.add("active");
 
-    const activeSlide = slides.querySelector("[data-active]");
-    let newIndex = [...slides.children].indexOf(activeSlide) + offset;
-    if (newIndex < 0) newIndex = slides.children.lenght - 1;
-    if (newIndex >= slides.children.length) newIndex = 0;
-
-    slides.children[newIndex].dataset.active = true;
-    delete activeSlide.dataset.active;
+    // Remove active class from all containers
+    containers.forEach((container) => {
+      container.classList.remove("active");
+    });
+    // Add active class to the container with the same index as the clicked sidebar element
+    containers[index].classList.add("active");
   });
 });
 
-const openInNewTab = (url) => {
-  window.open(url, "_blank");
-};
-
 /* ------------- TOGGLE EVENTS ------------- */
-const eTabs = document.querySelectorAll(".et");
-const ebox = document.querySelectorAll(".ec");
-const enav = document.querySelectorAll(".en");
+const oList = document.querySelectorAll(".otl");
+const tQuery = document.querySelector(".track-query");
+const closeTquery = document.querySelector(".close-tq");
 
 // Add a click event listener to each sidebar element
-eTabs.forEach((element, index) => {
+oList.forEach((element) => {
   element.addEventListener("click", () => {
-    // Remove active class from all sidebar elements
-    eTabs.forEach((el) => {
-      el.classList.remove("et-active");
-    });
-    // Add active class to the clicked sidebar element
-    element.classList.add("et-active");
-
-    // Remove active class from all containers
-    ebox.forEach((eb) => {
-      eb.classList.remove("ec-active");
-    });
-    // Add active class to the container with the same index as the clicked sidebar element
-    ebox[index].classList.add("ec-active");
-
-    // Remove active class from all containers
-    enav.forEach((en) => {
-      en.classList.remove("en-active");
-    });
-    // Add active class to the container with the same index as the clicked sidebar element
-    enav[index].classList.add("en-active");
+    tQuery.classList.add("active");
   });
+});
+
+closeTquery.addEventListener("click", () => {
+  tQuery.classList.remove("active");
 });
